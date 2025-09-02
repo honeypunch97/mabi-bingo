5f15d5bb38388176c1e085abfc7aa5701a24acdd
jongwoo <jongwoo@vinnsmail.com>
2025-09-02 09:28:32 +0900

    router 경로 수정

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,7 +1,7 @@
-import { createBrowserRouter } from "react-router-dom";
-import GlobalLayout from "../layouts/GlobalLayout";
-import BingoPage from "../pages/BingoPage";
-import MainPage from "../pages/MainPage";
+import { createBrowserRouter } from 'react-router-dom';
+import GlobalLayout from '../layouts/GlobalLayout';
+import BingoPage from '../pages/BingoPage';
+import MainPage from '../pages/MainPage';
 
 const router = createBrowserRouter([
   {
@@ -13,11 +13,11 @@ const router = createBrowserRouter([
         element: <MainPage />,
       },
       {
-        path:'bingo',
+        path: '/bingo',
         element: <BingoPage />,
-      }
-    ]
-  }
-])
+      },
+    ],
+  },
+]);
 
 export default router;
